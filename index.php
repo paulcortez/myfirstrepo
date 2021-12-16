f234580679e401d442d21bf8d7f7196d81338f5e
paulcortez <pbeldia@outlook.com>
2021-12-16 13:16:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Version 1.0</title>
+    <title>Document Version 2.0</title>
 </head>
 <body>
     <p>Welcome to my page Leo Beldia</p>
